ac7636b0319b340b2070e83bdbfcdee6c5305c36
xhinox <augusto.ferriz@gmail.com>
2015-08-08 13:31:25 -0700

    añadiendo archivos
    subiendo archivos de la primera clase

---
+++ js/javascript.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+	window.console.log('Esto es js en un archivo local');
+});
---
+++ js/min/javascript-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){window.console.log("Esto es js en un archivo local")});
